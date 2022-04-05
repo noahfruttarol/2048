d7a69cdac39f350d39274d9ff7796ad3b484a6e9
noahfruttarol <noahfruttarol@gmail.com>
2022-04-04 17:35:35 -0700

    formnatting

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 namespace _2048{
-    public class block
+    public class Block
     {
         //constructs block as empty.
-        public block()
+        public Block()
         {
             Value = 0;
-            been_merg = false;
+            Been_merg = false;
         }
 
 
@@ -15,41 +15,41 @@ namespace _2048{
         private int Value{ get; set; }
 
         //tell if block is already merged this turn
-        private bool been_merg { get; set; }
+        private bool Been_merg { get; set; }
 
         //gets the value of a block.
-        public int get_val()
+        public int Get_val()
         {
             return Value;
         }
 
         //sets block to other blcok if empty else doubles blocks value if they have the same val else returns 0
-        public int merg(ref block other)
+        public int Merg(ref Block other)
         {   //checks if block has been merged this turn as cannot not merg block twice in one turn and if other block has anything in it
-            if (been_merg || other.get_val() == 0)
+            if (Been_merg || other.Get_val() == 0)
                 return 0;//returns 0 to show block has not been moved
             //checks if block empty
-            if (get_val() == 0)
+            if (Get_val() == 0)
             {   //if block empty then sets block to other block and emptys other block
-                Value = other.get_val();
-                been_merg = other.been_merg;
-                other.clear();
-                return get_val(); //returns block value to show it's been moved
+                Value = other.Get_val();
+                Been_merg = other.Been_merg;
+                other.Clear();
+                return Get_val(); //returns block value to show it's been moved
             }
             //if block not empty but not same value, or other block is already been merged, then dont move other block and return 0 to show that nothing moved 
-            if (other.get_val() != get_val()||other.been_merg) 
+            if (other.Get_val() != Get_val()||other.Been_merg) 
                 return 0;
             //gets here if blocks have same value so they get merged and returns new value to show they have been merged
             Value *= 2;
-            other.clear();
-            been_merg = true;
-            return get_val();
+            other.Clear();
+            Been_merg = true;
+            return Get_val();
         }
 
         //sets block to 2 if empty 
-        public bool fill()
+        public bool Fill()
         {
-            if (get_val() != 0)
+            if (Get_val() != 0)
             {
                 return false;
             }
@@ -58,51 +58,51 @@ namespace _2048{
         }
 
         //sets block to 0 and been_merg to false
-        private void clear()
+        private void Clear()
         {
-            been_merg = false;
+            Been_merg = false;
             Value = 0;
         }
 
         //sets been_merg to false as its new turn
-        public void turn_reset() 
+        public void Turn_reset() 
         {
-            been_merg = false;
+            Been_merg = false;
         }
     }
     
-    public class borad 
+    public class Borad 
     { 
         //constructs with defalt size of 4
-        public borad()
+        public Borad()
         {
-            turns = 0;
+            Turns = 0;
             Size = 4;
-            end = 2048;
-            blocks = new block[Size, Size];
+            End = 2048;
+            Blocks = new Block[Size, Size];
             for (int i = 0; i < Size; i++)
             {
                 for (int j = 0; j < Size; j++)
                 {
-                    blocks[i, j] = new block();
+                    Blocks[i, j] = new Block();
                 }
             }
         }
 
         //constructs with size of input
-        public borad(int input)
+        public Borad(int input)
         {
-            turns = 0;
+            Turns = 0;
             if(input < 3 || input > 8) //borad is too small at 2 and would be too big at 9
                 throw new ArgumentOutOfRangeException("size too big should be between 3 and 8");
             Size = input;
-            end = 2048;
-            blocks = new block[Size, Size];
+            End = 2048;
+            Blocks = new Block[Size, Size];
             for (int i = 0; i < Size; i++)
             {
                 for (int j = 0; j < Size; j++)
                 {
-                    blocks[i, j] = new block();
+                    Blocks[i, j] = new Block();
                 }
             }
         }
@@ -111,22 +111,22 @@ namespace _2048{
         private int Size{ get; set; }
 
         //value if reached game ends
-        private int end { get; set; }
+        private int End { get; set; }
 
         //holds all the blocks in the borad
-        private block[,] blocks { get; set; }
+        private Block[,] Blocks { get; set; }
 
         //number of turns 
-        private int turns { get; set; }
+        private int Turns { get; set; }
 
         //starts the game
-        public void start()
+        public void Start()
         {
             Console.WriteLine("Starting game with {0} X {0} borad", Size);//starting text
             Console.WriteLine("Rules: w = up, s = down, a = left, d = right");//rules
             Console.WriteLine("Good Luck");
-            new_turn();//sets up borad with a block spawn
-            print_borad();
+            New_turn();//sets up borad with a block spawn
+            Print_borad();
             bool game_runing = true;//to see if the game is still in a valid state
             while (game_runing)
             {
@@ -138,16 +138,16 @@ namespace _2048{
                     c = s[0];//sets c to input if there is any
                 switch(c){
                     case 'w':
-                        up();
+                        Up();
                         break;
                     case 's':
-                        down();
+                        Down();
                         break;
                     case 'a':
-                        left();
+                        Left();
                         break;
                     case 'd':
-                        right();
+                        Right();
                         break;
                     default:
                         Console.WriteLine("Enter only a 'w', 'a', 's' or 'd'");
@@ -159,46 +159,46 @@ namespace _2048{
                     Console.Write("So, ");//invalid input skips does not add a turn
                     continue;
                 }
-                turns++;
-                game_runing = new_turn(); //checks for end states and spawns new block
+                Turns++;
+                game_runing = New_turn(); //checks for end states and spawns new block
                 if(game_runing)
-                    print_borad();
+                    Print_borad();
             }
         }
 
         //ends the game if 0 the player gets a block to the end amount or 1 if the borad is filled after the player made there turn
-        private void finish(int ending)
+        private void Finish(int ending)
         {
-            print_borad();
+            Print_borad();
             if (ending == 1)
             {
                 Console.WriteLine("Borad filled, no more room for blocks. Better luck next time.");
                 return;//failed ending 
             }
-            Console.WriteLine("Congrats you won in {0} turns, good job!", turns);//sucsess ending
+            Console.WriteLine("Congrats you won in {0} turns, good job!", Turns);//sucsess ending
         }
 
         //prints borad to console
-        private void print_borad()
+        private void Print_borad()
         {
             Console.Write("_");
             for (int i = 0; i < Size; i++)
                 Console.Write("_____");//prints top line
             for (int i = 0; i < Size; i++)
             {
-                print_row(i);
+                Print_row(i);
             }
         }
 
         //prints row to console
-        private void print_row(int row) 
+        private void Print_row(int row) 
         {
             
             Console.WriteLine();
             Console.Write("|");//prints left most line
             for (int i = 0; i < Size; i++)
             {
-                set_color(row, i); //sets coulor of block
+                Set_color(row, i); //sets coulor of block
                 Console.Write("    |");
             }
             Console.ResetColor();
@@ -206,9 +206,9 @@ namespace _2048{
             Console.Write("|");//next loop the same exsept adds value of block  
             for (int i = 0; i < Size; i++)
             {
-                set_color(row, i);
-                if(blocks[row, i].get_val() != 0)
-                    Console.Write("{0,-4}|", blocks[row, i].get_val());
+                Set_color(row, i);
+                if(Blocks[row, i].Get_val() != 0)
+                    Console.Write("{0,-4}|", Blocks[row, i].Get_val());
                 else
                     Console.Write("    |");//if empty block no number
             }
@@ -217,17 +217,17 @@ namespace _2048{
             Console.Write("|");
             for (int i = 0; i < Size; i++)
             {
-                set_color(row, i);
+                Set_color(row, i);
                 Console.Write("____|");//prints bottom of box
             }
             Console.ResetColor();
             Console.WriteLine("");
         }
 
-        private void set_color(int row, int col)
+        private void Set_color(int row, int col)
         {
-            block temp = blocks[row, col];
-            switch (temp.get_val())//sets color based on value of block
+            Block temp = Blocks[row, col];
+            switch (temp.Get_val())//sets color based on value of block
             {
                 case 0:
                     Console.ResetColor();
@@ -261,7 +261,7 @@ namespace _2048{
         }
 
         //resets been_merged in each block and spawns a new block on the borad
-        private bool new_turn()
+        private bool New_turn()
         {   //gets array to hold indexes of empty blocks
             int[] temp = new int[Size * Size];
             int temp_index = 0;//amount of empty blocks
@@ -270,34 +270,34 @@ namespace _2048{
             {
                 for (int j = 0; j < Size; j++)
                 {
-                    if (blocks[i, j].get_val() == 0)
+                    if (Blocks[i, j].Get_val() == 0)
                     {   //empty block added to temp so it may be filled 
                         temp[temp_index] = i * Size + j;//stores index as i*size + j so just one int to worry about
                         temp_index++;
                     }
                     else
                     {   //else ends game if it is the end value
-                        if (blocks[i, j].get_val() == end)
+                        if (Blocks[i, j].Get_val() == End)
                         {
-                            finish(0);
+                            Finish(0);
                             return false;//returns false if end of game
                         }
-                        blocks[i, j].turn_reset(); //resets been merged as it is new turn
+                        Blocks[i, j].Turn_reset(); //resets been merged as it is new turn
                     }
                 }
             }
             if (temp_index == 0)
             {
-                finish(1); //no empty blocks therefore end of game 
+                Finish(1); //no empty blocks therefore end of game 
                 return false;//end of game returns false
             }
             var rand = new Random();
             int index = temp[rand.Next(0, temp_index)]; //gets random index of empty block
-            blocks[index/Size, index%Size].fill(); //filles the block
+            Blocks[index/Size, index%Size].Fill(); //filles the block
             return true;//returns true if game still going
         }
 
-        private void up() 
+        private void Up() 
         { 
             for(int col = 0; col < Size; col++)
             {   //loop to go over each column
@@ -306,7 +306,7 @@ namespace _2048{
                     int k = i;//k to keep track of the block to move up
                     for (int j = i - 1; j >= 0; j--)//j keeps track of block being merged with
                     {   //loop to keep moving the block up untill it cant be merged
-                        int temp = blocks[j, col].merg(ref blocks[k, col]);//merges block at row k with block above 
+                        int temp = Blocks[j, col].Merg(ref Blocks[k, col]);//merges block at row k with block above 
                         if (temp == 0)//if temp is 0 then the block was not moved and there can no longer be a merg from block at row k
                             break;
                         k--;
@@ -315,7 +315,7 @@ namespace _2048{
             }
         }
 
-        private void down()
+        private void Down()
         {   
             for (int col = 0; col < Size; col++)
             {   //loop to go over each column
@@ -324,7 +324,7 @@ namespace _2048{
                     int k = i;//k to keep track of the block to move down
                     for (int j = i + 1; j < Size; j++)//j keeps track of block being merged with
                     {   //loop to keep moving the block down untill it cant be merged
-                        int temp = blocks[j, col].merg(ref blocks[k, col]);//merges block at row k with block below
+                        int temp = Blocks[j, col].Merg(ref Blocks[k, col]);//merges block at row k with block below
                         if (temp == 0)//if temp is 0 then the block was not moved and there can no longer be a merg from block at row k
                             break;
                         k++;
@@ -333,7 +333,7 @@ namespace _2048{
             }
         }
 
-        private void left()
+        private void Left()
         { 
             for (int row = 0; row < Size; row++)
             {   //loop to go over each row
@@ -342,7 +342,7 @@ namespace _2048{
                     int k = i;//k to keep track of the block to move left
                     for (int j = i - 1; j >= 0; j--)//j keeps track of block being merged with
                     {   //loop to keep moving the block left untill it cant be merged
-                        int temp = blocks[row, j].merg(ref blocks[row, k]);//merges block at column k with block left
+                        int temp = Blocks[row, j].Merg(ref Blocks[row, k]);//merges block at column k with block left
                         if (temp == 0)//if temp is 0 then the block was not moved and there can no longer be a merg from block at column k
                             break;
                         k--;
@@ -351,7 +351,7 @@ namespace _2048{
             }
         }
 
-        private void right()
+        private void Right()
         { 
             for (int row = 0; row < Size; row++)
             {   //loop to go over each row
@@ -360,7 +360,7 @@ namespace _2048{
                     int k = i;//k to keep track of the block to move right
                     for (int j = i + 1; j < Size; j++)//j keeps track of block being merged with
                     {   //loop to keep moving the block right untill it cant be merged
-                        int temp = blocks[row, j].merg(ref blocks[row, k]);//merges block at column k with block right
+                        int temp = Blocks[row, j].Merg(ref Blocks[row, k]);//merges block at column k with block right
                         if (temp == 0)//if temp is 0 then the block was not moved and there can no longer be a merg from block at column k
                             break;
                         k++;
@@ -389,13 +389,13 @@ namespace _2048{
             int x = Convert.ToInt32(Console.ReadLine());
             if (x >= 3 || x <= 8)
             {
-                borad game = new borad(x);
-                game.start();
+                Borad game = new Borad(x);
+                game.Start();
             }
             else//if invalid size use defalt
             {
-                borad game = new borad();
-                game.start();
+                Borad game = new Borad();
+                game.Start();
             }
             return 0;
         }
